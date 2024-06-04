80a2079a1af0ff7a2d4eec85ef16a3c717b0f5e1
DSM3 <fatec-dsm3@academico.intra>
2024-06-04 20:53:22 -0300

    Atualizando Telas de Perfil e Dados

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapa').setView([-24.5031, -47.8431], 14);
+var mymap = L.map('mapa').setView([-24.5031, -47.8431], 12);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -6,4 +6,8 @@ var mymap = L.map('mapa').setView([-24.5031, -47.8431], 14);
 
         L.marker([-24.4674, -47.8919]).addTo(mymap)
             .bindPopup('Sítio A')
+            .openPopup();
+
+        L.marker([-24.4244,-47.9402]).addTo(mymap)
+            .bindPopup('Sítio B')
             .openPopup();
