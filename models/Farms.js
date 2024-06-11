f46b02de22a4a5cabb83c988f70bffa58f532ca9
DSM3 <fatec-dsm3@academico.intra>
2024-06-10 22:53:01 -0300

    Cards e Pagina de dados concluida

--- models/Farms.js
+++ models/Farms.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const Farms = new mongoose.Schema({
     num_plantas : Number,
-    dt_analise : Date,
+    dt_analise : String,
     localizacao : String,
     tamanho : Number,
     tipo_pupunheira : String,
